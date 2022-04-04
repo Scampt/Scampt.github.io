b8e65eaa7648512528be576bd86f40d5770a7b52
unknown <falcony45@gmail.com>
2022-04-03 19:50:44 -0500

    Portafolio agregado

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let escribir = new Typed(".auto-texto", {
+    strings: ["I'm Luis K. Quiroz Pacheco", "I'm a Web Developer", "I'm a UI Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
